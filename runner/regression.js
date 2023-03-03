fbd6528f736ef3bb6561739a5781a0859ac205b1
hadiindrawan <hadiindrawan157@gmail.com>
2023-03-03 10:05:11 +0700

    fix obj mapping

--- runner/regression.js
+++ runner/regression.js
@@ -1,3 +1,3 @@
 // Write your runner in this file
-require('../tests/scenarios/Users/addfile.spec')()
+require('../tests/scenarios/Users/createuser.spec')()
 // require('../tests/Auth/login.spec')()
