af5e3ef0f7fe1dc037a7fe76898fe0c0faa6e60b
hadiindrawan <hadiindrawan157@gmail.com>
2024-02-07 10:21:30 +0700

    fix: change entry point condition

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { PogenCommand } from "command/pogen.command"
 const argument = process.argv[process.argv.length - 1];
 const pogenCommand = new PogenCommand(argument);
 
-if (argument === 'generate' || argument === '' || argument.includes('/.bin/generate') || argument.includes('/po-gen/lib') ) {
+if (argument === 'generate' || argument === '' || argument.includes('/.bin/generate') || argument.includes('\\po-gen\\lib') ) {
     console.log('Initiating automation generation');
     pogenCommand.automation();
 } else if (argument === 'env-generate') {
