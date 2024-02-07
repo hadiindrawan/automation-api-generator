cc869a6ca18426c1afdbf3b35ac3d261188c999d
hadiindrawan <hadiindrawan157@gmail.com>
2024-02-07 09:09:53 +0700

    fix: entry point condition

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,12 @@ import { PogenCommand } from "command/pogen.command"
 const argument = process.argv[process.argv.length - 1];
 const pogenCommand = new PogenCommand(argument);
 
-switch (argument) {
-    case 'generate':
-    case '':
-        console.log('Initiating automation generation');
-        pogenCommand.automation();
-        break;
-    case 'env-generate':
-        console.log('Initiating environment generation');
-        pogenCommand.environment();
-        break;
-    default:
-        console.log(`Unknown argument: ${argument}`);
+if (argument === 'generate' || argument === '' || argument.includes('/.bin/generate')) {
+    console.log('Initiating automation generation');
+    pogenCommand.automation();
+} else if (argument === 'env-generate') {
+    console.log('Initiating environment generation');
+    pogenCommand.environment();
+} else {
+    console.log(`Unknown argument: ${argument}`);
 }
